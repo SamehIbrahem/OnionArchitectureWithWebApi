b7e5acf4b148e95d0152a0e020755d9fac99cd5b
Sameh Ibrahem <sameh.ifi@gmail.com>
2019-01-19 02:27:38 +0200

    configure the start up

--- SmartTechApi/Startup.cs
+++ SmartTechApi/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Microsoft.Owin;
+using Microsoft.Owin.Cors;
 using Owin;
 using SmartTechApi.Infrastructure;
 
@@ -12,7 +13,7 @@ namespace SmartTechApi
     {
         public void Configuration(IAppBuilder app)
         {
-            // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=316888
+            app.UseCors(CorsOptions.AllowAll);
             AutoMapperConfiguration.Init();
         }
     }
